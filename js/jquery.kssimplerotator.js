effdb17f2b7722d5cd629edaff9f70fcc55e256d
Kevin Sheffield <sheffield.k@gmail.com>
2012-05-11 13:08:32 -0400

    checking in transitions : slide and fade for now

--- js/jquery.kssimplerotator.js
+++ js/jquery.kssimplerotator.js
@@ -1,5 +1,5 @@
 (function( $ ){
-  var defaults = {time: 3000};
+  var defaults = {time: 3000, transition: 'fade'};
   var images, currentRotator = 0, t, itemCount;
   $.fn.simpleRotator = function(options) {
     options = $.extend({}, defaults, options);
@@ -7,15 +7,26 @@
     itemCount = images.length;
     images.css("display","none");
     $(images[currentRotator]).css("display","block");
-    t = setTimeout("$().cycleRotator(" + options.time + ")", options.time);
+    t = setTimeout("$().cycleRotator(" + options.time + ", '" + options.transition + "')", options.time);
     return this;
   };
-  $.fn.cycleRotator = function(time) {
+  $.fn.cycleRotator = function(time, transition) {
 	var oldOne = $(images[currentRotator]);
 	currentRotator = (currentRotator + 1) % itemCount;
-	$(images[currentRotator]).fadeIn("fast");
-	oldOne.css("display","none");
-	t = setTimeout("$().cycleRotator(" + time + ")", time);
+  switch(transition)
+  {
+    case "fade":
+      $(images[currentRotator]).fadeIn("fast");
+      oldOne.css("display","none");
+      break;
+    case "slide":
+      $(images[currentRotator]).slideDown("1000");
+      oldOne.slideUp("1000");
+      break;
+    default:
+      break;
+  }
+	t = setTimeout("$().cycleRotator(" + time + ", '" + transition + "')", time);
   };
 
 })( jQuery );
